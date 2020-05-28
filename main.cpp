85230b8ee54e6012f1d4d57a01765d85b9edc134
Daniel Klein <ehmeth@gmail.com>
2020-05-28 12:40:01 +0200

    Lock-free implementation approach

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <windows.h>
 #include <strsafe.h>
 #include <stdint.h>
+#include <atomic>
 
 #define USE_STRETCH_DI_BITS 0
 #define USE_LIDKA_PRED 0
@@ -257,19 +258,19 @@ typedef void (*job_handler_t)(void *);
 
 enum job_status_t {
    JOB_FREE,
+   JOB_IN_PREP,
    JOB_AVAILABLE,
    JOB_PENDING
 };
 
 struct job_spec_t {
-   job_status_t status;
+   std::atomic<job_status_t>status;
    job_handler_t handler;
    uint8_t params[JOB_QUEUE_PARAMS_SIZE];
 };
 
 static struct {
    HANDLE work_event;
-   CRITICAL_SECTION cs;
    bool running;
    job_spec_t specs[JOB_QUEUE_SIZE];
 } job_queue;
@@ -279,23 +280,13 @@ DWORD job_queue_worker(void * param) {
    {
       WaitForSingleObject(job_queue.work_event, INFINITE);
       
-      bool found = false;
-      uint32_t job_index;
-      EnterCriticalSection(&job_queue.cs);
-      for (auto i = 0; (i < JOB_QUEUE_SIZE) && !found; i++)
+      for (auto i = 0; i < JOB_QUEUE_SIZE; i++)
       {
-         if (job_queue.specs[i].status == JOB_AVAILABLE) {
-            job_queue.specs[i].status = JOB_PENDING;
-            found = true;
-            job_index = i;
+         job_status_t expected = JOB_AVAILABLE;
+         if (job_queue.specs[i].status.compare_exchange_strong(expected, JOB_PENDING)) {
+            job_queue.specs[i].handler(job_queue.specs[i].params);
+            job_queue.specs[i].status.store(JOB_FREE, std::memory_order_release);
          }
-      }
-      LeaveCriticalSection(&job_queue.cs);
-      if (found) {
-         job_queue.specs[job_index].handler(job_queue.specs[job_index].params);
-         EnterCriticalSection(&job_queue.cs);
-         job_queue.specs[job_index].status = JOB_FREE;
-         LeaveCriticalSection(&job_queue.cs);
       }
    }
 
@@ -305,7 +296,6 @@ DWORD job_queue_worker(void * param) {
 void job_queue_init()
 {
    job_queue.work_event = CreateEvent(NULL, TRUE, FALSE, NULL);
-   InitializeCriticalSection(&job_queue.cs);
    job_queue.running = true;
    for (auto i = 0; i < JOB_QUEUE_SIZE; i++)
    {
@@ -330,19 +320,18 @@ void job_queue_push(job_handler_t handler, void * data, uint32_t data_len)
    bool queued = false;
    // OutputDebugStringA("job pushed");
 
-   EnterCriticalSection(&job_queue.cs);
    for (auto i = 0; (i < JOB_QUEUE_SIZE) && !queued; i++)
    {
-      if (job_queue.specs[i].status == JOB_FREE) {
+      job_status_t expected = JOB_FREE;
+      if (job_queue.specs[i].status.compare_exchange_strong(expected, JOB_IN_PREP)) {
          job_queue.specs[i].handler = handler;
          memcpy_s(job_queue.specs[i].params, JOB_QUEUE_PARAMS_SIZE, data, data_len);
-         job_queue.specs[i].status = JOB_AVAILABLE;
+         job_queue.specs[i].status.store(JOB_AVAILABLE, std::memory_order_release);
          queued = true;
          // OutputDebugStringA("job queued");
          SetEvent(job_queue.work_event);
       }
    }
-   LeaveCriticalSection(&job_queue.cs);
 
    if (!queued) {
       handler(data);
@@ -359,42 +348,28 @@ void job_queue_wait_until_done()
    while (!all_started)
    {
       all_started = true;
-      bool found = false;
-      uint32_t job_index;
 
-      EnterCriticalSection(&job_queue.cs);
-      for (auto i = 0; (i < JOB_QUEUE_SIZE) && !found; i++)
+      for (auto i = 0; (i < JOB_QUEUE_SIZE); i++)
       {
-         if (job_queue.specs[i].status == JOB_AVAILABLE) {
-            job_queue.specs[i].status = JOB_PENDING;
-            found = true;
+         job_status_t expected = JOB_AVAILABLE;
+         if (job_queue.specs[i].status.compare_exchange_strong(expected, JOB_PENDING)) {
             all_started = false;
-            job_index = i;
-         } 
-      }
-      LeaveCriticalSection(&job_queue.cs);
-
-      if (found) {
-         job_queue.specs[job_index].handler(job_queue.specs[job_index].params);
-         EnterCriticalSection(&job_queue.cs);
-         job_queue.specs[job_index].status = JOB_FREE;
-         LeaveCriticalSection(&job_queue.cs);
+            job_queue.specs[i].handler(job_queue.specs[i].params);
+            job_queue.specs[i].status.store(JOB_FREE, std::memory_order_release);
+         }
       }
    }
 
    ResetEvent(job_queue.work_event);
    while (!all_done) {
       all_done = true;
-      EnterCriticalSection(&job_queue.cs);
       for (auto i = 0; (i < JOB_QUEUE_SIZE) && all_done; i++)
       {
-         if (job_queue.specs[i].status != JOB_FREE) {
+         if (job_queue.specs[i].status.load(std::memory_order_acquire) != JOB_FREE) {
             all_done = false;
          } 
       }
-      LeaveCriticalSection(&job_queue.cs);
    }
-
 }
 
 struct chunk_spec_t {
